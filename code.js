866cb5a4eba4036a2a53a471eb1f6622a54ca8d4
Brian Harrison <brianharrison.2002@gmail.com>
2021-07-23 10:34:58 -0400

    add to css and format date

--- code.js
+++ code.js
@@ -11,44 +11,40 @@ const root = document.getElementById("root");
       const firstName = document.createElement("h2");
       firstName.innerText = customers[i].name.first;
       const lastName = customers[i].name.last;
-      firstName.innerText += " " + lastName;
+     firstName.innerText += " " + lastName;
       root.appendChild(firstName);
       console.log(firstName);
     
       
-      const email = document.createElement("h3");
-      email.classList.add("email-address");
-      email.innerText = customers[i].email;
-      customerCard.appendChild(email);
+       const email = document.createElement("h3");
+       email.innerText = customers[i].email;
+      root.appendChild(email);
       console.log(email.innerText)
 
 
       const address = document.createElement("h3");
-      address.classList.add("address");
+      
       address.innerText = customers[i].location.street.number;
       const streetName = customers[i].location.street.name;
       address.innerText += " " + streetName;
-      customerCard.appendChild(email);
+      root.appendChild(email);
       console.log(address.innerText)
 
       const address2 = document.createElement("h3");
-      address2.classList.add("address");
       address2.innerText = customers[i].location.city + ",";
       address2.innerText += " " + nameToAbbr(customers[i].location.state);
       address2.innerText += " " + customers[i].location.postcode;
-      customerCard.appendChild(address2);
+      root.appendChild(address2);
       console.log(address2.innerText);
 
        const birthday = document.createElement("h3");
-       birthday.classList.add("birtday");
        birthday.innerText = customers[i].dob.date;
-       customerCard.appendChild(birthday);
+       root.appendChild(birthday);
        console.log(birthday.innerText);
 
        const employed = document.createElement("h3");
-       employed.classList.add("birthday");
        employed.innerText = customers[i].registered.date;
-       customerCard.appendChild(employed);
+       root.appendChild(employed);
        console.log(employed.innerText);
 
 
@@ -58,18 +54,5 @@ const root = document.getElementById("root");
    
 
 
-// function addElement () {
-  // create a new div element
-  // const newDiv = document.createElement("div");
-
-  // and give it some content
-   // const newContent = document.createTextNode("Hi there and greetings!");
-
-  // add the text node to the newly created div
-  // newDiv.appendChild(newContent);
-
-  // add the newly created element and its content into the DOM
-  // const currentDiv = document.getElementById("div1");
-  // document.body.insertBefore(newDiv, currentDiv);
 
 
